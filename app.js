ebb23a38bfb62d0c1076b6d50854dbc46c0e711f
darshpandya02 <pandya.dar@northeastern.edu>
2023-12-04 21:34:56 -0500

    a6 changes

--- app.js
+++ app.js
@@ -17,9 +17,9 @@ import ModuleRoutes from "./modules/routes.js";
 import UserRoutes from "./users/routes.js";
 import cors from "cors";
 import mongoose from "mongoose";
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+const CONNECTION_STRING =
+  process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
 mongoose.connect(CONNECTION_STRING);
-
 
 const app = express();
 app.use(
